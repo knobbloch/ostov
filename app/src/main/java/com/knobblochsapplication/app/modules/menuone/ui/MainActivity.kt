0d029fee1a54d453edc7a871b8e8f0e1f878f061
morgen-muffel57 <115986051+morgen-muffel57@users.noreply.github.com>
2023-06-15 19:29:38 +0500

    Add fragment for create tasks and dialog windows

--- app/src/main/java/com/knobblochsapplication/app/modules/menuone/ui/MainActivity.kt
+++ app/src/main/java/com/knobblochsapplication/app/modules/menuone/ui/MainActivity.kt
@@ -6,12 +6,16 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.elevation.SurfaceColors
 import com.knobblochsapplication.app.R
 import com.knobblochsapplication.app.appcomponents.utility.PreferenceHelper
 import com.knobblochsapplication.app.databinding.ActivityMainMenuBinding
 import com.knobblochsapplication.app.modules.diagramview.ui.DiagramViewActivity
 import com.knobblochsapplication.app.modules.downloadlist.ui.DownloadListActivity
+import com.knobblochsapplication.app.modules.goals.ui.CreateGoalDialogFragment
 import com.knobblochsapplication.app.modules.goals.ui.GoalsActivity
 import com.knobblochsapplication.app.modules.helpscreenone.ui.HelpScreenOneActivity
 import com.knobblochsapplication.app.modules.settings.ui.SettingsActivity
@@ -51,8 +55,68 @@ class MainActivity : AppCompatActivity() {
                     startActivity(myIntent)
                     true
                 }
+
                 else -> false
             }
+        }
+        binding.include.addGoalBtn.setOnClickListener {
+
+            MaterialAlertDialogBuilder(this)
+                .setNeutralButton(R.string.lbl21) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setItems(R.array.dialog_actions_with_task) { dialog, which ->
+                    when (which) {
+                        0 -> {
+                            val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+                            ft.addToBackStack(null)
+                            val newFragment: DialogFragment = CreateTaskDialogFragment.newInstance()
+                            newFragment.show(ft, "dialog")
+
+                            dialog.dismiss()
+                        }
+
+                        1 -> {
+                            dialog.dismiss()
+                        }
+
+                        2 -> {
+                            MaterialAlertDialogBuilder(this)
+                                .setNegativeButton(R.string.lbl21) { dialog, _ ->
+                                    dialog.dismiss()
+                                }
+                                .setPositiveButton(R.string.lbl22) { dialog, _ ->
+                                    dialog.dismiss()
+                                }
+                                .setSingleChoiceItems(
+                                    R.array.dialog_delete_task,
+                                    0
+                                ) { dialog, which ->
+                                    when (which) {
+                                        0 -> {
+
+                                        }
+
+                                        1 -> {
+
+                                        }
+                                    }
+                                }
+                                .setTitle(R.string.lbl34)
+                                .show()
+                            dialog.dismiss()
+
+                        }
+
+                        3 -> {
+                            dialog.dismiss()
+
+                        }
+                    }
+                }
+                .setTitle(R.string.change_action)
+                .create()
+                .show()
         }
     }
 
