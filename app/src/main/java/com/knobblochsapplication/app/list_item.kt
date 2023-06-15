8197c1abad1c3d8d84b3971afb9a1a4bf99ed7fc
morgen-muffel57 <115986051+morgen-muffel57@users.noreply.github.com>
2023-06-15 18:41:34 +0500

    Add menu for work with goal scheme

---
+++ app/src/main/java/com/knobblochsapplication/app/list_item.kt
@@ -0,0 +1,5 @@
+package com.knobblochsapplication.app
+
+data class list_item (
+    var image_Id: Int
+        )
---
+++ app/src/main/java/com/knobblochsapplication/app/modules/goals/ui/GoalsAdapter.kt
@@ -0,0 +1,58 @@
+package com.knobblochsapplication.app.modules.goals.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.knobblochsapplication.app.R
+import com.knobblochsapplication.app.databinding.GoalItemBinding
+
+class GoalsAdapter(val listener: Listener, private val goalsList: ArrayList<Goal>) :
+    RecyclerView.Adapter<GoalsAdapter.GoalHolder>() {
+
+
+    class GoalHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val binding = GoalItemBinding.bind(item)
+
+
+        fun bind(goal: Goal, listener: Listener) = with(binding) {
+            txtNamegoal.text = goal.goalName
+            txtAboutgoal.text = goal.goalDescription
+            deadline.text = goal.goalDeadline
+            itemView.setOnClickListener {
+                listener.onGoalClick(adapterPosition)
+            }
+            btnDelete.setOnClickListener {
+                listener.onBtnDeleteClick(adapterPosition)
+            }
+            btnEdit.setOnClickListener {
+                listener.onBtnEditClick(adapterPosition)
+            }
+            btnChangeLevel.setOnClickListener {
+                listener.onBtnChangeLevelClick(adapterPosition)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.goal_item, parent, false)
+        return GoalHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return goalsList.size
+    }
+
+    override fun onBindViewHolder(holder: GoalHolder, position: Int) {
+        holder.bind(goalsList[position], listener)
+    }
+
+
+    interface Listener {
+        fun onGoalClick(position: Int)
+        fun onBtnDeleteClick(position: Int)
+        fun onBtnEditClick(position: Int)
+        fun onBtnChangeLevelClick(position: Int)
+    }
+
+}
