5243a6e340ad0e91ca482ddfe6e1105c60c90090
Facenip <122119233+Facenip@users.noreply.github.com>
2023-06-16 11:25:27 +0500

    Back_change

--- app/src/main/java/com/knobblochsapplication/app/modules/File_system/by_complete.java
+++ app/src/main/java/com/knobblochsapplication/app/modules/File_system/by_complete.java
@@ -7,19 +7,17 @@ import java.util.Comparator;
 
 public class by_complete implements Comparator<Integer> {
 
-    Context context;
     Integer goal_id;
-    public by_complete(Context _context, int _goal_id) {
-        context=_context;
+    public by_complete(int _goal_id) {
         goal_id=_goal_id;
     }
 
     public int compare(Integer o1, Integer o2) {
         try {
-            if(File_Manager.Find_task_by_id(context,goal_id,o1).isComplete()==File_Manager.Find_task_by_id(context,goal_id,o2).isComplete()){
+            if(File_Manager.Find_task_by_id(goal_id,o1).isComplete()==File_Manager.Find_task_by_id(goal_id,o2).isComplete()){
                 return 0;
             }
-            if(File_Manager.Find_task_by_id(context, goal_id, o1).isComplete() && !File_Manager.Find_task_by_id(context, goal_id, o2).isComplete()){
+            if(File_Manager.Find_task_by_id(goal_id, o1).isComplete() && !File_Manager.Find_task_by_id(goal_id, o2).isComplete()){
                 return -1;
             }
             else{
